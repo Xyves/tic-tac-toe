67e1de19abfb546aaccfdf9a7378b62e2963cc49
Xyves <xodrixx@protonmail.com>
2023-09-26 13:56:52 +0200

    Check for a winner

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { board, updateBoard, win, isGameActive, result } from "./modules.js";
+import { board, updateBoard, isGameActive, result } from "./modules.js";
 
 const cells = Array.from(document.querySelectorAll(".cell"));
 const statusText = document.querySelector(".statusText");
@@ -17,31 +17,57 @@ for (let i = 0; i < cells.length; i++) {
 
 function isValidAction() {
   const index = this.getAttribute("cellIndex");
-  if (cells[index].textContent == "X" || cells[index].textContent == "O") {
+  if (cells[index].textContent !== "") {
     return false;
-  } else {
-    takeTurn(index);
   }
+  takeTurn(index);
 }
-function isGameWon() {
-  // Porównaj Object(board) z Object(win) jeżeli tak, log wygrałeś = currentPlayer
-}
+
 function takeTurn(index) {
-  if (isGameActive) {
+  if (isGameActive && isGameWon) {
     cells[index].textContent = currentPlayer;
     updateBoard(index, currentPlayer);
-    isGameWon();
-    switchPlayer();
+    if (!isGameWon(board)) {
+      switchPlayer();
+    } else {
+      let board = null;
+      console.log(board);
+    }
   } else {
     return error;
   }
 }
 
+function isGameWon(board) {
+  let winner;
+  const winningPatterns = [
+    [board[0], board[1], board[2]],
+    [board[3], board[4], board[5]],
+    [board[6], board[7], board[8]],
+    [board[0], board[3], board[6]],
+    [board[1], board[4], board[7]],
+    [board[2], board[5], board[8]],
+    [board[0], board[4], board[8]],
+    [board[2], board[4], board[6]],
+  ];
+
+  for (const condition of winningPatterns) {
+    // Player X wins
+    if (condition.every((element) => element === "X")) {
+      console.log(`player1 has won!`);
+      return true;
+    }
+    // Player O wins
+    if (condition.every((element) => element === "O")) {
+      console.log(`player2 has won!`);
+      return true;
+    }
+  }
+  return winner;
+}
 function switchPlayer() {
   if (currentPlayer == player1) {
-    console.log(player1 + "X");
     currentPlayer = player2;
-    console.log("text");
   } else if (currentPlayer == player2) {
     currentPlayer = player1;
   } else {
